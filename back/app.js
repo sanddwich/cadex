a04c067f10c63489f0965af11cdfa0e63ba9a2d1
Denis <64834314+sanddwich@users.noreply.github.com>
2020-12-18 10:02:51 +0400

    32132132

--- back/app.js
+++ back/app.js
@@ -1,23 +1,29 @@
-const CONFIG = require('./config')
-const express = require('express')
+const CONFIG = require("./config");
+var cors = require("cors");
 
-
+const express = require("express");
 const app = express();
+app.use(cors());
 
-const serverUrl = CONFIG.server.url + ':' + CONFIG.server.port.toString()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+const serverUrl = CONFIG.server.url + ":" + CONFIG.server.port.toString();
 
 //  Functions ===============================================
-
 
 //  ROUTES =====================================
 
 //  Main route
-app.get('/api', (req, res) => {
-  console.log('GET Request')
-  res.send(JSON.stringify({message: 'hello'}))
-})
+app.post("/api", (req, res) => {
+  console.log('POST query');
+  res.send(JSON.stringify(req.body));
+});
 
 //  SERVER listen
 app.listen(CONFIG.server.port, () => {
-  console.log('APP listen port: ' + CONFIG.server.port)
-})
+  console.log("APP listen port: " + CONFIG.server.port);
+});
--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import './App.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: 0,
+      height: 1,
+      length: 2,
+    }
+  }
+
   getApi = async () => {
     const res = await fetch('http://localhost:3001/api', {
-      method: 'GET',
+      method: 'POST',
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin:": "*",
-      }
+      },
+      body: JSON.stringify(this.state)
     })
 
-
     if(res.ok) {
-      console.log(await res.text())
+      console.log(await res.json())
     } else {
       console.log('API ERROR')
     }
@@ -21,10 +30,26 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Triangle</h1>
-        <button onClick={() => this.getApi()}>GetApi</button>
-      </div>
+      <Container fluid className="p-0 App">
+        <Row className="m-0 p-0">
+          <Col sm={3} className="m-0 p-0">
+            <div className="App__formContainer">
+              <div className="App__input">
+                <input type="number" name="wigth" placeholder="|Ширина"></input>
+              </div>
+              <div className="App__input">
+                <input type="number" name="length" placeholder="|Длина"></input>
+              </div>
+              <div className="App__input">
+                <input type="number" name="height" placeholder="|Высота"></input>
+              </div>
+              <div className="App__button">
+                <button onClick={() => this.getApi()}>Смоделировать</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
   
